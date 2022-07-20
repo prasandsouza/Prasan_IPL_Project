6cfffb52beb7f3c627afac61d836dcbb9b686dca
prasandsouza <prasan.dsouza.20.2@mountblue.tech>
2022-07-20 14:35:47 +0530

    updated function for second problem

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -36,4 +36,35 @@ FileSystem.writeFile('/home/prasan/IPL/src/public/output/matchesPerTeam.json', J
 });
 
 
+function winnerperseason(){
+    return matchData.reduce((finalValue, currentValue) => {
+        if(currentValue['id']!==''){
+         if (finalValue[currentValue.winner]) {
+              if (finalValue[currentValue.winner][currentValue.season]) {
+                   finalValue[currentValue.winner][currentValue.season]++;
+              } else {
+                   finalValue[currentValue.winner][currentValue.season] = 1;
+              }
+         }
+         else{
+            finalValue[currentValue.winner] = {}
+            finalValue[currentValue.winner][currentValue.season] = 1;
+         }
+        }
+         return finalValue;
+    }, {});
+    }
+    const secondProblem = winnerperseason()
+    
+    FileSystem.writeFile('/home/prasan/IPL/src/public/output/winningPerSeason.json', JSON.stringify(secondProblem), (data, error) => {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            console.log("File statement.");
+        }
+    });
 
+
+
+    
